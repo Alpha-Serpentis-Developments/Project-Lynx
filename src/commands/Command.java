46b5fcaf0ec7386027b663d8a2f5a5ef7572cf15
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2019-06-19 17:25:50 -0500

    Update Command.java

--- src/commands/Command.java
+++ src/commands/Command.java
@@ -1,38 +1,63 @@
 package commands;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+
+import init.Launcher;
+import net.dv8tion.jda.core.entities.Role;
+import net.dv8tion.jda.core.entities.TextChannel;
 
 public abstract class Command {
 
 	/*
 	 * cmdName is REQUIRED
 	 * cmdType is REQUIRED, otherwise it DEFAULTS to "GENERAL"
+	 * Leave cmdPerms empty if you want EVERYONE to use it.
 	 */
 	private String cmdName, cmdDesc, cmdType;
-	private HashMap<String, Integer> cmdPerms; 
+	private ArrayList<Long> cmdPerms = new ArrayList<Long>();
 	
+	/**NOT RECOMMENDED, PLEASE DEFINE YOUR COMMANDS EARLY AT INIT OR IN THE CODE ELSEWHERE
+	 * 
+	 * @param n is the name
+	 */
 	public Command(String n) {
 		setName(n);
 		setDesc("Use the [setDesc] command to set this command's description!");
 		setType("GENERAL");
 	}
+	/**NOT RECOMMENDED, PLEASE DEFINE YOUR COMMANDS EARLY AT INIT OR IN THE CODE ELSEWHERE
+	 * 
+	 * @param n is the name
+	 * @param d is the description
+	 */
 	public Command(String n, String d) {
 		setName(n);
 		setDesc(d);
 		setType("GENERAL");
 	}
+	/**NOT RECOMMENDED, PLEASE DEFINE YOUR COMMANDS EARLY AT INIT OR IN THE CODE ELSEWHERE
+	 * 
+	 * @param n is the name
+	 * @param d is the description
+	 * @param t is the type
+	 */
 	public Command(String n, String d, String t) {
 		setName(n);
 		setDesc(d);
 		setType(t);
 	}
-	public Command(String n, String d, HashMap<String, Integer> cp) {
+	/**
+	 * 
+	 * @param n
+	 * @param d
+	 * @param cp
+	 */
+	public Command(String n, String d, ArrayList<Long> cp) {
 		setName(n);
 		setDesc(d);
 		setPerms(cp);
 		setType("GENERAL");
 	}
-	
 	
 	//Setter Methods
 	public void setName(String n) {
@@ -41,7 +66,7 @@ public abstract class Command {
 	public void setDesc(String d) {
 		cmdDesc = d;
 	}
-	public void setPerms(HashMap<String, Integer> p) {
+	public void setPerms(ArrayList<Long> p) {
 		cmdPerms = p;
 	}
 	public void setType(String t) {
@@ -53,9 +78,20 @@ public abstract class Command {
 		return cmdName;
 	}
 	public String getDesc() {
+		
+		if(cmdPerms.size() != 0) {
+			
+			String returnThis = cmdDesc + "\n\n**Roles Needed**: ";
+			for(long id: cmdPerms) {
+				returnThis += Launcher.api.getRoleById(id).getName()+", ";
+			}
+			
+			return returnThis.substring(0, returnThis.length() - 2);
+		}
+		
 		return cmdDesc;
 	}
-	public HashMap<String, Integer> getRoleIDs() {
+	public ArrayList<Long> getRoleIDs() {
 		return cmdPerms;
 	}
 	public String getType() {
@@ -63,21 +99,24 @@ public abstract class Command {
 	}
 	
 	//Misc Methods
-	public void addPerm(String id, int val) {
-		
-	}
-	public void addPerm(HashMap<String, Integer> p) {
-		
-	}
-	
-	public void removePerm(String id, int val) {
-		
-	}
-	public void removePerm(HashMap<String, Integer> p) {
+
+	public void addPerm(long id) {
 		
 	}
 	
-	public abstract boolean action(HashMap<String, Boolean> p);
-	public abstract String toString();
+
+	public void removePerm(long id) {
+		
+	}
+	
+	/**
+	 * Define action() as you wish in classes that extend this.
+	 * @param chn is used to allow sending messages to a certain text channel
+	 * @param msg is used to carry around messages (you do not have to use msg if you don't wish)
+	 * @param misc is optional to use, otherwise you can pass a null value
+	 * 
+	 * @return true if successful, otherwise false
+	 */
+	public abstract boolean action(TextChannel chn, String msg, Object misc);
 	
 }
