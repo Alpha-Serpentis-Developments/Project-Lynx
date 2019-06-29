f01a9b14374644aa6dd3d5b99cc61573851a2461
AlphaSerpentis <arandomserpentis@gmail.com>
2019-06-28 20:00:09 -0500

    Update Launcher.java
    Created initOverride() and fixed the System.exit(-1); line within the locationKey if statement

--- src/main/java/init/Launcher.java
+++ src/main/java/init/Launcher.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 import javax.security.auth.login.LoginException;
 
+import data.Data;
 import handlers.MessageHandler;
 import handlers.ServerHandler;
 import net.dv8tion.jda.core.JDA;
@@ -23,6 +24,12 @@ public class Launcher {
 
 	public Launcher() {
 
+		//TESTING, REMOVE THIS
+
+		Data.initCache();
+
+		//TESTING END
+
 		Scanner sc;
 
 		/*
@@ -31,156 +38,30 @@ public class Launcher {
 		if(InitData.locationKey.isEmpty()) {
 
 			System.out.println("Key's location is empty! Checking if there is an override for key's location?");
-
-			System.exit(-1);
+			//OVERRIDE INITIALIZATION
+			overrideInit();
 
 		} else {
+			//OVERRIDE INITIAZLIATION
+			overrideInit();
+
+			//JDA INITIALIZATION
+			System.out.println("Starting up JDA initialization...");
 
 			try {
-				System.out.println("Initializing...");
-
-				//OVERRIDE SCANNER
-				sc = new Scanner(new File("resources/initOverrides.txt")); // Checks for overrides to be applied
-
-				if(sc.hasNext())
-					System.out.println("Override(s) detected, scanning...");
-				else
-					System.out.println("No overrides found, using default settings.");
-
-				while(sc.hasNext()) {
-
-					String itm = sc.nextLine(), ident = itm.substring(0, itm.indexOf("(")), stuff = itm.substring(itm.indexOf("(") + 1, itm.indexOf(")"));
-
-					//System.out.println(itm + " " + ident + " " + stuff);
-
-					for(String oKey: InitData.overrideKeys) {
-
-						if(oKey.equals(ident)) {
-
-							System.out.println("Applying " + oKey + " override");
-
-							switch(ident) {
-
-							case "locKey":
-								InitData.locationKey = stuff;
-								break;
-							case "guildID":
-								InitData.guildID = stuff;
-								break;
-							case "logID":
-								InitData.logID = stuff;
-								break;
-							case "botOwnerIDs":
-
-								Scanner scb = new Scanner(stuff);
-								List<Long> ids = new ArrayList<Long>();
-								scb.useDelimiter(",");
-								while(scb.hasNext()) {
-									ids.add(scb.nextLong());
-								}
-
-								InitData.botOwnerIDs = new Long[ids.size()];
-								for(int i = 0; i < ids.size(); i++) {
-									InitData.botOwnerIDs[i] = ids.get(i);
-								}
-
-								break;
-							case "modIDs":
-
-								scb = new Scanner(stuff);
-								ids = new ArrayList<Long>();
-								scb.useDelimiter(",");
-								while(scb.hasNext()) {
-									ids.add(scb.nextLong());
-								}
-
-								InitData.modIDs = new Long[ids.size()];
-								for(int i = 0; i < ids.size(); i++) {
-									InitData.modIDs[i] = ids.get(i);
-								}
-
-								break;
-							case "adminIDs":
-
-								scb = new Scanner(stuff);
-								ids = new ArrayList<Long>();
-								scb.useDelimiter(",");
-								while(scb.hasNext()) {
-									ids.add(scb.nextLong());
-								}
-
-								InitData.adminIDs = new Long[ids.size()];
-								for(int i = 0; i < ids.size(); i++) {
-									InitData.adminIDs[i] = ids.get(i);
-								}
-
-								break;
-							case "permLvl":
-								System.out.println("permLvl override is unused! Nothing changed");
-								break;
-							case "prefix":
-								if(stuff.isEmpty() || stuff.length() > 1) {
-									System.out.println("prefix override is malformed! Nothing changed");
-								} else {
-									InitData.prefix = stuff.charAt(0);
-								}
-								break;
-							case "accptPrv":
-								InitData.acceptPriv = Boolean.parseBoolean(stuff);
-								break;
-							case "vers":
-								InitData.version = stuff;
-								break;
-
-							}
-
-						}
-
-					}
-
-				}
-
-				//JDA INITIALIZATION
-				System.out.println("Starting up JDA initialization...");
-
 				sc = new Scanner(new File(InitData.locationKey));
 
 				JDAInit(sc.next());
 
 				sc.close();
-
+			} catch (LoginException | InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			} catch (FileNotFoundException e) {
-				System.out.println("File error! Error: ");
-
+				// TODO Auto-generated catch block
 				e.printStackTrace();
-
-				System.exit(-1);
-			} catch (LoginException e) {
-				System.out.println("Login Failure! Error: ");
-
-				api.shutdownNow();
-
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				System.out.println("Error: ");
-
-				api.shutdownNow();
-
-				e.printStackTrace();
-			} catch (IndexOutOfBoundsException e) {
-				System.out.println("Possibly malformed initOverrides! Error: ");
-
-				e.printStackTrace();
-
-				System.out.println("Overrides ignored, using default settings.");
-				try {
-					ignoreOverride();
-				} catch (InterruptedException | FileNotFoundException | LoginException e1) {
-
-					e1.printStackTrace();
-				}
-
 			}
+
 		}
 
 	}
@@ -224,6 +105,121 @@ public class Launcher {
 		System.out.println("Initializing complete!");
 		api.getPresence().setGame(Game.playing("Looking for food..."));
 
+	}
+
+	public void overrideInit() {
+
+		try {
+			System.out.println("Initializing...");
+
+			//OVERRIDE SCANNER
+			Scanner sc;
+
+			sc = new Scanner(new File("resources/initOverrides.txt")); // Checks for overrides to be applied
+
+			if(sc.hasNext())
+				System.out.println("Override(s) detected, scanning...");
+			else
+				System.out.println("No overrides found, using default settings.");
+
+			while(sc.hasNext()) {
+
+				String itm = sc.nextLine(), ident = itm.substring(0, itm.indexOf("(")), stuff = itm.substring(itm.indexOf("(") + 1, itm.indexOf(")"));
+
+				//System.out.println(itm + " " + ident + " " + stuff);
+
+				for(String oKey: InitData.overrideKeys) {
+
+					if(oKey.equals(ident)) {
+
+						System.out.println("Applying " + oKey + " override");
+
+						switch(ident) {
+
+						case "locKey":
+							InitData.locationKey = stuff;
+							break;
+						case "guildID":
+							InitData.guildID = stuff;
+							break;
+						case "logID":
+							InitData.logID = stuff;
+							break;
+						case "botOwnerIDs":
+
+							Scanner scb = new Scanner(stuff);
+							List<Long> ids = new ArrayList<Long>();
+							scb.useDelimiter(",");
+							while(scb.hasNext()) {
+								ids.add(scb.nextLong());
+							}
+
+							InitData.botOwnerIDs = new Long[ids.size()];
+							for(int i = 0; i < ids.size(); i++) {
+								InitData.botOwnerIDs[i] = ids.get(i);
+							}
+
+							break;
+						case "modIDs":
+
+							scb = new Scanner(stuff);
+							ids = new ArrayList<Long>();
+							scb.useDelimiter(",");
+							while(scb.hasNext()) {
+								ids.add(scb.nextLong());
+							}
+
+							InitData.modIDs = new Long[ids.size()];
+							for(int i = 0; i < ids.size(); i++) {
+								InitData.modIDs[i] = ids.get(i);
+							}
+
+							break;
+						case "adminIDs":
+
+							scb = new Scanner(stuff);
+							ids = new ArrayList<Long>();
+							scb.useDelimiter(",");
+							while(scb.hasNext()) {
+								ids.add(scb.nextLong());
+							}
+
+							InitData.adminIDs = new Long[ids.size()];
+							for(int i = 0; i < ids.size(); i++) {
+								InitData.adminIDs[i] = ids.get(i);
+							}
+
+							break;
+						case "permLvl":
+							System.out.println("permLvl override is unused! Nothing changed");
+							break;
+						case "prefix":
+							if(stuff.isEmpty() || stuff.length() > 1) {
+								System.out.println("prefix override is malformed! Nothing changed");
+							} else {
+								InitData.prefix = stuff.charAt(0);
+							}
+							break;
+						case "accptPrv":
+							InitData.acceptPriv = Boolean.parseBoolean(stuff);
+							break;
+						case "vers":
+							InitData.version = stuff;
+							break;
+
+						}
+
+					}
+
+				}
+
+			}
+
+			sc.close();
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void getServers() {
