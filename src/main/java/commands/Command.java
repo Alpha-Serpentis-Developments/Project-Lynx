1bbf2bdaa9e314783953bedbf9325c8eecf2b2ff
AlphaSerpentis <arandomserpentis@gmail.com>
2019-07-01 18:12:59 -0500

    Update Command.java
    This is to help catch up with the gradle-test branch

--- src/main/java/commands/Command.java
+++ src/main/java/commands/Command.java
@@ -1,7 +1,9 @@
 package commands;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
+import init.InitData;
 import init.Launcher;
 import net.dv8tion.jda.core.entities.TextChannel;
 
@@ -11,22 +13,31 @@ public abstract class Command {
 	 * cmdName is REQUIRED
 	 * cmdType is REQUIRED, otherwise it DEFAULTS to "GENERAL"
 	 * cmdPerms can be left empty, however if requirePerms = true, you'll need to configure.
+	 * allowPrivate determines if the command can be used in the DMs
 	 */
 	private String cmdName, cmdDesc, cmdType, cmdUsage;
-	private ArrayList<Long> cmdPerms = new ArrayList<Long>();
-	private boolean requirePerms = false;
-	
+	private HashMap<String, ArrayList<Long>> cmdPerms = new HashMap<String, ArrayList<Long>>();
+	private boolean requirePerms = false, allowPrivate = true;
+
+	//TODO: Fix these constructors...
+
 	/**NOT RECOMMENDED, PLEASE DEFINE YOUR COMMANDS EARLY AT INIT OR IN THE CODE ELSEWHERE
-	 * 
+	 *
 	 * @param n is the name
 	 */
 	public Command(String n) {
 		setName(n);
-		setDesc("Use the [setDesc] command to set this command's description!");
+		setDesc("Use the `" + InitData.prefix + "setDesc [description]` command to set this command's description!");
 		setType("GENERAL");
 	}
+	public Command(String n, boolean rqrPrm) {
+		setName(n);
+		setDesc("Use the `" + InitData.prefix + "setDesc [description]` command to set this command's description!");
+		setType("GENERAL");
+		setRequirePerms(rqrPrm);
+	}
 	/**NOT RECOMMENDED, PLEASE DEFINE YOUR COMMANDS EARLY AT INIT OR IN THE CODE ELSEWHERE
-	 * 
+	 *
 	 * @param n is the name
 	 * @param d is the description
 	 */
@@ -36,7 +47,7 @@ public abstract class Command {
 		setType("GENERAL");
 	}
 	/**NOT RECOMMENDED, PLEASE DEFINE YOUR COMMANDS EARLY AT INIT OR IN THE CODE ELSEWHERE
-	 * 
+	 *
 	 * @param n is the name
 	 * @param d is the description
 	 * @param t is the type
@@ -47,18 +58,39 @@ public abstract class Command {
 		setType(t);
 	}
 	/**
-	 * 
+	 *
 	 * @param n
 	 * @param d
 	 * @param cp
 	 */
-	public Command(String n, String d, ArrayList<Long> cp) {
+	public Command(String n, String d, HashMap<String, ArrayList<Long>> cp) {
 		setName(n);
 		setDesc(d);
 		setPerms(cp);
 		setType("GENERAL");
 	}
-	
+	public Command(String n, String d, HashMap<String, ArrayList<Long>> cp, String type) {
+		setName(n);
+		setDesc(d);
+		setPerms(cp);
+		setType(type);
+	}
+	public Command(String n, String d, HashMap<String, ArrayList<Long>> cp, String type, boolean rqrPrm) {
+		setName(n);
+		setDesc(d);
+		setPerms(cp);
+		setType(type);
+		setRequirePerms(rqrPrm);
+	}
+	public Command(String n, String d, HashMap<String, ArrayList<Long>> cp, String type, boolean rqrPrm, boolean allwPrv) {
+		setName(n);
+		setDesc(d);
+		setPerms(cp);
+		setType(type);
+		setRequirePerms(rqrPrm);
+		setAllowPrivate(allwPrv);
+	}
+
 	//Setter Methods
 	public void setName(String n) {
 		cmdName = n;
@@ -66,7 +98,7 @@ public abstract class Command {
 	public void setDesc(String d) {
 		cmdDesc = d;
 	}
-	public void setPerms(ArrayList<Long> p) {
+	public void setPerms(HashMap<String, ArrayList<Long>> p) {
 		cmdPerms = p;
 	}
 	public void setType(String t) {
@@ -78,26 +110,43 @@ public abstract class Command {
 	public void setUsage(String s) {
 		cmdUsage = s;
 	}
-	
+	public void setAllowPrivate(boolean b) {
+		allowPrivate = b;
+	}
+
 	//Getter Methods
 	public String getName() {
 		return cmdName;
 	}
+
 	public String getDesc() {
-		
+
 		if(cmdPerms.size() != 0) {
-			
+
 			String returnThis = cmdDesc + "\n\n**Roles Needed**: ";
-			for(long id: cmdPerms) {
-				returnThis += Launcher.api.getRoleById(id).getName()+", ";
+			if(cmdPerms.get("ROLE") != null) {
+				for(long id: cmdPerms.get("ROLE")) {
+					returnThis += Launcher.api.getRoleById(id).getName() + ", ";
+				}
+
+				returnThis.substring(0,  returnThis.length() - 2);
 			}
-			
-			return returnThis.substring(0, returnThis.length() - 2);
+
+			if(cmdPerms.get("USER") != null) {
+				returnThis += "\n**Users Exempted**: ";
+				for(long id: cmdPerms.get("USER")) {
+					returnThis += Launcher.api.getUserById(id).getAsTag() + ", ";
+				}
+
+				 returnThis.substring(0,  returnThis.length() - 2);
+			}
+			return returnThis;
 		}
-		
+
 		return cmdDesc;
+
 	}
-	public ArrayList<Long> getRoleIDs() {
+	public HashMap<String, ArrayList<Long>> getRoleIDs() {
 		return cmdPerms;
 	}
 	public String getType() {
@@ -106,27 +155,45 @@ public abstract class Command {
 	public boolean getRequirePerms() {
 		return requirePerms;
 	}
-	public String setUsage() {
+	public String getUsage() {
 		return cmdUsage;
 	}
-	
+	public boolean getAllowPrivate() {
+		return allowPrivate;
+	}
+
 	//Misc Methods
 
-	public void addPerm(long id) {
-		cmdPerms.add(id);
+	public void addPerm(String key, long id) throws Exception {
+		if(!key.equalsIgnoreCase("USER") || !key.equalsIgnoreCase("ROLE")) {
+			throw new Exception("Malformed command permission key!");
+		}
+
+		cmdPerms.get(key).add(id);
 	}
-	public void removePerm(long id) {
-		cmdPerms.remove(id);
+	public void removePerm(String key, long id) throws Exception {
+		if(!key.equalsIgnoreCase("USER") || !key.equalsIgnoreCase("ROLE")) {
+			throw new Exception("Malformed command permission key!");
+		}
+
+		cmdPerms.get(key).remove(id);
 	}
-	
+	/**
+	 * Used in conjunction if requirePerms is true, otherwise this is redundant!
+	 * @return true if cmdPerms is defined, otherwise false
+	 */
+	public boolean isRoleIDsDefined() {
+		return !cmdPerms.isEmpty();
+	}
+
 	/**
 	 * Define action() as you wish in classes that extend this.
 	 * @param chn is used to allow sending messages to a certain text channel
 	 * @param msg is used to carry around messages (you do not have to use msg if you don't wish)
 	 * @param misc is optional to use, otherwise you can pass a null value
-	 * 
+	 *
 	 * @return true if successful, otherwise false
 	 */
 	public abstract boolean action(TextChannel chn, String msg, Object misc);
-	
+
 }
