6ac14a1eea55de253ddcf33f0aa219ac22e5594b
AlphaSerpentis <arandomserpentis@gmail.com>
2019-07-16 05:30:44 -0500

    Added enum CommandType for `java/commands`

---
+++ src/main/java/commands/CommandType.java
@@ -0,0 +1,17 @@
+package commands;
+
+public enum CommandType {
+
+	GENERAL("general"), MOD("mod"), GUILD_OWNER("guild_owner"), BOT_OWNER("bot_owner");
+
+	private String name;
+
+	CommandType(String s) {
+		this.name = s;
+	}
+
+	public String toString() {
+		return name;
+	}
+
+}
