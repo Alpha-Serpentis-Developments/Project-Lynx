ec0f51b803e49e880b944472d49f997aa83a5a42
AlphaSerpentis <arandomserpentis@gmail.com>
2019-08-12 21:52:14 -0500

    Rewrite secondary cache fix #2

--- src/main/java/handlers/MessageHandler.java
+++ src/main/java/handlers/MessageHandler.java
@@ -21,17 +21,17 @@ public class MessageHandler implements EventListener {
 
 	@Override
 	public void onEvent(Event event) {
-	
+
 		//System.out.println("DEBUG [MessageHandler.java]: " + event);
-		
+
 		/*
 		 * Checks if the event triggered is a message type and ISN'T A BOT (you can remove it from the if statement if you wish).
 		 */
 		if((event instanceof MessageReceivedEvent || (InitData.acceptPriv && event instanceof PrivateMessageReceivedEvent)) && !((MessageReceivedEvent) event).getAuthor().isBot() && Launcher.initialized) {
-			
+
 			Guild g = ((MessageReceivedEvent) event).getGuild();
 			char prefix; //Server's prefix... if it's even a server.
-			
+
 			if(Data.srvr_cache.get(g) == null && g != null) {
 				Data.addGuild(g);
 				prefix = InitData.prefix;
@@ -44,45 +44,45 @@ public class MessageHandler implements EventListener {
 			 * Checks the message uses the defined prefix found in InitData.java (you can change the prefix if you need to)
 			 */
 			if(((MessageReceivedEvent) event).getMessage().getContentRaw().indexOf(prefix) == 0) {
-				
+
 				String fullMsg = ((MessageReceivedEvent) event).getMessage().getContentRaw().substring(1);
 				ChannelType c = event instanceof PrivateMessageReceivedEvent ? ChannelType.PRIVATE : ChannelType.TEXT;
-				
+
 				Command cmd;
 				if(!fullMsg.contains(" ")) {
-					cmd = CommandHandler.getCommand(fullMsg);
+					cmd = CommandHandler.getCommand(fullMsg, g);
 				} else {
-					cmd = CommandHandler.getCommand(fullMsg.substring(0, fullMsg.indexOf(" ")));
+					cmd = CommandHandler.getCommand(fullMsg.substring(0, fullMsg.indexOf(" ")), g);
 				}
-				
+
 				if(cmd == null || (g == null && cmd.getRequirePerms() == true)) return;
-				
+
 				System.out.println("DEBUG [MessageHandler.java]: " + cmd.getName());
 				System.out.println("DEBUG [MessageHandler.java]: (ChannelType) " + c);
-				
+
 				if(c.equals(ChannelType.PRIVATE))
 					cmd.action(((MessageReceivedEvent) event).getAuthor().openPrivateChannel().complete(), fullMsg, event); //Just pass the entire thing to prevent NullPointers, each command will handle them appropriately
 				else if(c.equals(ChannelType.TEXT))
 					cmd.action(((MessageReceivedEvent) event).getChannel(), fullMsg, event);
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	public static void sendMessage(MessageChannel chn, String s) {
-		
+
 		Consumer<Message> callback = (response) -> System.out.printf("[MessageHandler.java] Sent \"%s\"", response);
 		try {
 			chn.sendMessage(s).queue(callback);
 		} catch(InsufficientPermissionException e) {
 			System.out.println("[MessageHandler.java]: Message was not sent due to insufficient permissions!");
 		}
-		
+
 	}
-	
+
 	public static void sendMessage(MessageChannel chn, String s, File f) {
-		
+
 		if(f != null) {
 			Consumer<Message> callback = (response) -> System.out.printf("[MessageHandler.java] Sent ", response);
 			try {
@@ -92,12 +92,12 @@ public class MessageHandler implements EventListener {
 			}
 		} else
 			System.out.println("Unable to send a message, file doesn't exist?");
-		
+
 	}
-	
+
 	//TODO: Do this...
 	public void embedMessage() {
-		
+
 	}
-	
+
 }
