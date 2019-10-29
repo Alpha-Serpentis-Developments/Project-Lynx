ad84bc1ffdfbee8e640f731e2d05f7470e1f74f8
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2019-10-29 07:15:17 -0500

    Update Configure.java (unfinished)
    UNFINISHED!
    
    Begin work on Configure.java, one of the last few commands to work on to allow guild owners to tailor their needs.

--- src/main/java/commands/utilities/Configure.java
+++ src/main/java/commands/utilities/Configure.java
@@ -14,27 +14,40 @@ public class Configure extends Command {
 
 	@Override
 	public boolean action(MessageChannel chn, String msg, Object misc) {
-		
+
+		String[] configKeys = {"ban", "kick", "warn", "warnings", "welcome"};
+
 		Guild gld = ((MessageReceivedEvent) misc).getGuild();
 		User usr = ((MessageReceivedEvent) misc).getAuthor();
-		
-		boolean result = ServerHandler.getServerOwner(gld.getIdLong()).equals(usr);
-		String type;
-		
+
+		boolean result = ServerHandler.getServerOwner(gld.getIdLong()).equals(usr); //TODO: Change this to allow various type of users and set the server owner as the default.
+		String type, config;
+
+		//Checks what type of message to send
 		if(msg.substring(0, msg.indexOf(" ")).equalsIgnoreCase("config")) {
-			
+			MessageHandler.sendMessage(chn, getDesc());
+			return true;
 		} else {
-			
+
 		}
-		
+
 		if(result) {
-			
-			
-			
+
+			config = msg.substring(msg.indexOf(" ") + 1).substring(0, msg.indexOf(" ")); //Supposed to find the key?
+
+			for(String k: configKeys) {
+				if(k.equalsIgnoreCase(config)) {
+
+					//Write code...
+
+					return true;
+				}
+			}
+
 		}
-		
+
 		return false;
-		
+
 	}
-	
+
 }
