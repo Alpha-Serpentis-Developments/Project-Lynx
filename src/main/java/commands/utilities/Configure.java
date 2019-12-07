03a07ef0ecb97aafe7caa63997bd88e1111c434e
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2019-12-06 21:04:58 -0600

    Put the enum back into Configure.java

--- src/main/java/commands/utilities/Configure.java
+++ src/main/java/commands/utilities/Configure.java
@@ -1,6 +1,10 @@
 package commands.utilities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import commands.Command;
+import commands.Commands;
 import data.Data;
 import handlers.MessageHandler;
 import handlers.ServerHandler;
@@ -9,6 +13,30 @@ import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+enum Commands {
+	
+	// This will suffice for now lol
+	
+	ABOUT("name", 0),
+	HELP("help", 1);
+	
+	private static List<Commands> commands = new ArrayList<Commands>();
+	
+	private String assignmentName = "DEFAULT";
+	private int assignmentNum = -1;
+	
+	Commands(String n, int i) {
+		
+	}
+	
+	public int getCommandAssignmentNum(String n) {
+
+		
+		
+		return -1;
+	}
+}
 
 public class Configure extends Command {
 	
