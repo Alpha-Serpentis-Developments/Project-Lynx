f74c360fc353ae42bc5eae66e3bd150e6ae93821
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2019-12-06 20:25:42 -0600

    Updated conf and added enum
    Updated configure to remove the enum, and instead made the enum its own file

--- src/main/java/commands/utilities/Configure.java
+++ src/main/java/commands/utilities/Configure.java
@@ -11,53 +11,30 @@ import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 public class Configure extends Command {
-
+	
 	@Override
 	public boolean action(MessageChannel chn, String msg, Object misc) {
-
-		String[] configKeys = {"ban", "kick", "warn", "warnings", "welcome"};
-
+		
 		Guild gld = ((MessageReceivedEvent) misc).getGuild();
 		User usr = ((MessageReceivedEvent) misc).getAuthor();
-
-		boolean result = ServerHandler.getServerOwner(gld.getIdLong()).equals(usr); //TODO: Change this to allow various type of users and set the server owner as the default.
-		String type, config;
-
-		//Checks what type of message to send
+		
+		boolean result = ServerHandler.getServerOwner(gld.getIdLong()).equals(usr);
+		String type;
+		
 		if(msg.substring(0, msg.indexOf(" ")).equalsIgnoreCase("config")) {
-			MessageHandler.sendMessage(chn, getDesc());
-			return true;
+			
 		} else {
-
+			
 		}
-
+		
 		if(result) {
-
-			config = msg.substring(msg.indexOf(" ") + 1).substring(0, msg.indexOf(" ")); //Supposed to find the key?
-
-			for(String k: configKeys) {
-
-				switch(k.toLowerCase()) {
-
-					default:
-						return true;
-
-				}
-
-				/*
-				if(k.equalsIgnoreCase(config)) { //Actually, replace this with a switch statement.
-
-					//Write code...
-
-					return true;
-				}
-				*/
-			} //for loop
-
-		} //if statement
-
+			
+			
+			
+		}
+		
 		return false;
-
+		
 	}
-
+	
 }
