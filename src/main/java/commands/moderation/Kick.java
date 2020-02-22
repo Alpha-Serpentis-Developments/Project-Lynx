f3cd3880fdeecf15ec7f5c84a9025d83f9849c61
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2020-02-21 23:26:03 -0600

    Updated commands.moderation.Kick

--- src/main/java/commands/moderation/Kick.java
+++ src/main/java/commands/moderation/Kick.java
@@ -2,8 +2,12 @@ package commands.moderation;
 
 import commands.Command;
 import handlers.MessageHandler;
+import init.InitData;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.GuildChannel;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
@@ -13,7 +17,9 @@ public class Kick extends Command {
 	public boolean action(MessageChannel chn, String msg, Object misc) {
 		
 		Guild gld = ((MessageReceivedEvent) misc).getGuild();
-		User mod = ((MessageReceivedEvent) misc).getAuthor(), punished = (msg.length() == getName().length()) ? null : gld.getMemberById(msg.substring(msg.indexOf("@") + 1, msg.indexOf(">"))).getUser();
+		User mod = ((MessageReceivedEvent) misc).getAuthor(), punished = (msg.length() == getName().length()) ? null : gld.getMemberById(msg.substring(msg.indexOf("!") + 1, msg.indexOf(">"))).getUser();
+		
+		String reason = msg.substring(msg.indexOf(">") + 1);
 		
 		if(punished == null) {
 			MessageHandler.sendMessage(chn, getDesc());
@@ -23,7 +29,8 @@ public class Kick extends Command {
 		boolean result = verifyExecution(mod, punished, gld, chn);
 		
 		if(result) {
-			
+			gld.kick(gld.getMember(punished), reason).queue();
+			MessageHandler.sendMessage(gld.getTextChannelById(InitData.logID), "Moderator **" + mod.getAsTag() + "** has kicked user **" + punished.getAsTag() + "** for: \n\n> " + reason);
 		}
 		
 		return false;
