c074ffbe0d65db48ecc82f90c4d546c3070166ca
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2020-08-12 23:28:45 -0500

    Minor (unfinished) additions to `updateMovingAverage()`

--- Project-Lynx/src/main/java/features/urp/UserRaidProtection.java
+++ Project-Lynx/src/main/java/features/urp/UserRaidProtection.java
@@ -10,21 +10,33 @@ import net.dv8tion.jda.api.hooks.EventListener;
 public class UserRaidProtection implements EventListener {
 	
 	public static volatile HashMap<Guild, URP_Telemetry> moving_avg = new HashMap<Guild, URP_Telemetry>();
-	public static final Double baseUserThreshold = 5.00; 
-	public static final String detectedRoleName = "URP Triggered";
+	public static final Double baseUserThreshold = 3.00; 
+	public static final String detectedRoleName = "Lynx URP Triggered";
 	
+	/**
+	 * This method is called every time a new user joins, so the count goes up by one.
+	 * However, this method recalculates the moving average to account for the new user, but also checks for a new valid UNIX timestamp to calculate the moving average.
+	 * 
+	 * @param gld is the Guild to update.
+	 * @return a Double value with the new moving average for the guild.
+	 */
 	public static Double updateMovingAverage(Guild gld) {
 		
-		long currentInst = Instant.now().getEpochSecond();
-		long telemetryInst = moving_avg.get(gld).getLastUNIX();
+		URP_Telemetry g_urp = moving_avg.get(gld);
 		
-		long difference = currentInst - telemetryInst;
-		// Check if it has been an hour
-		if(difference > 60 * 60) {
-			moving_avg.get(gld).setLastUNIX(currentInst);
+		Long currentUNIX = Instant.now().getEpochSecond();
+		Double currentMA = g_urp.getMovingAvg();
+		Integer currentUserCount;
+		
+		// Check if the UNIX is still valid
+		if(currentUNIX - g_urp.getLastAcceptableUNIX() > 60 * 60) { //60 sec * 60 minutes 
+			
+			// Check the pairs if anyone of them are not within the 3600 seconds.
+			
 		}
 		
 		// Reformulate as necessary
+		
 		
 		return null;
 		
