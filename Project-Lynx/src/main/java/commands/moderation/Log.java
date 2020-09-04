a62a8af8d4ed80efb7ed2d07ddb7a9097963aa03
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2020-09-03 21:40:32 -0500

    Unfinished Build 6
    - Preparation to replace special configurations with dedicated commands.

---
+++ Project-Lynx/src/main/java/commands/moderation/Log.java
@@ -0,0 +1,16 @@
+package commands.moderation;
+
+import commands.Command;
+import net.dv8tion.jda.api.entities.MessageChannel;
+
+public class Log extends Command {
+
+	@Override
+	public boolean action(MessageChannel chn, String msg, Object misc) {
+		
+		
+		
+		return false;
+	}
+
+}
---
+++ Project-Lynx/src/main/java/commands/moderation/URP.java
@@ -0,0 +1,14 @@
+package commands.moderation;
+
+import commands.Command;
+import net.dv8tion.jda.api.entities.MessageChannel;
+
+public class URP extends Command {
+
+	@Override
+	public boolean action(MessageChannel chn, String msg, Object misc) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
