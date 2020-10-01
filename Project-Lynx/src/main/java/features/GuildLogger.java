f3141efdbf8a5cfa092fdefa875cf79cd475697c
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2020-10-01 17:45:02 -0500

    Expanded GuildLogger (unfinished)

--- Project-Lynx/src/main/java/features/GuildLogger.java
+++ Project-Lynx/src/main/java/features/GuildLogger.java
@@ -6,11 +6,13 @@ import java.util.List;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.guild.GenericGuildEvent;
+import net.dv8tion.jda.api.events.guild.GuildBanEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 
 public class GuildLogger implements EventListener {
 	
-	public static volatile HashMap<Guild, List<GenericGuildEvent>> gldLogEvents = new HashMap<Guild, List<GenericGuildEvent>>();
+	public static volatile HashMap<Guild, List<GenericGuildEvent>> gldLogJDAEvents = new HashMap<Guild, List<GenericGuildEvent>>();
+	public static volatile HashMap<Guild, List<String>> gldLogCustomEvents = new HashMap<Guild, List<String>>();
 	
 	public void customEvent(String k) {
 		
@@ -21,41 +23,80 @@ public class GuildLogger implements EventListener {
 		
 		if(event instanceof GenericGuildEvent) {
 			
-			GenericGuildEvent genericEvt = (GenericGuildEvent) event;
-			
-			if(!gldLogEvents.containsKey(genericEvt.getGuild())) // If the guild isn't in the HashMap, get out.
-				return;
-			
-			// Loop the list
-			for(GenericGuildEvent gEvnt: gldLogEvents.get(genericEvt.getGuild())) {
-				
-				switch(gEvnt.getClass().getSimpleName().toLowerCase()) {
-				
-				// JDA Events
-				case "guildbanevent":
+			new Thread() {
+				public void run() {
 					
-					break;
-				case "guildkickevent":
+					GenericGuildEvent genericEvt = (GenericGuildEvent) event;
 					
-					break;
-				case "guildmessagedeleteevent":
+					if(!gldLogJDAEvents.containsKey(genericEvt.getGuild())) // If the guild isn't in the HashMap, get out.
+						return;
 					
-					break;
+					// Loop the list
+					for(GenericGuildEvent gEvnt: gldLogJDAEvents.get(genericEvt.getGuild())) {
+						
+						switch(gEvnt.getClass().getSimpleName().toLowerCase()) {
+						
+						// JDA Events
+						case "guildbanevent":
+							
+							GBan((GuildBanEvent) gEvnt);
+							
+							break;
+						case "guildkickevent":
+							
+							GKick();
+							
+							break;
+						case "guildmessagedeleteevent":
+							
+							GMessageDelete();
+							
+							break;
+							
+						// Custom Events
+						case "warn":
+							
+							Warn();
+							
+							break;
+						case "userraiddetect":
+							
+							UserRaid();
+							
+							break;
+						
+						}
+						
+					}
 					
-				// Custom Events
-				case "warn":
-					
-					break;
-				case "userraiddetect":
-					
-					break;
-				
 				}
-				
-			}
+			}.run();
 			
 		}
 		
+	}
+	
+	public boolean GBan(GuildBanEvent evt) {
+		
+		
+		
+		return false;
+	}
+	
+	public boolean GKick() {
+		return false;
+	}
+	
+	public boolean GMessageDelete() {
+		return false;
+	}
+	
+	public boolean Warn() {
+		return false;
+	}
+	
+	public boolean UserRaid() {
+		return false;
 	}
 	
 }
