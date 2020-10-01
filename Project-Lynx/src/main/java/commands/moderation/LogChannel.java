69c0440c97e61f56e5e10351c78b4a30e7e80511
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2020-09-30 23:34:15 -0500

    LogChannel command functioning (Build 6 not ready)

--- Project-Lynx/src/main/java/commands/moderation/LogChannel.java
+++ Project-Lynx/src/main/java/commands/moderation/LogChannel.java
@@ -1,7 +1,14 @@
 package commands.moderation;
 
+import org.json.JSONObject;
+
 import commands.Command;
+import data.Data;
+import handlers.MessageHandler;
+import init.InitData;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class LogChannel extends Command {
 	
@@ -11,8 +18,39 @@ public class LogChannel extends Command {
 
 	@Override
 	public boolean action(MessageChannel chn, String msg, Object misc) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		if(msg.equalsIgnoreCase("logchannel")) {
+			MessageHandler.sendMessage(chn, getDesc());
+			return true;
+		} else if(!verifyUse(((MessageReceivedEvent) misc).getAuthor(), ((MessageReceivedEvent) misc).getGuild(), chn)) {
+			return false;
+		}
+		
+		String modifyThis = new String(msg);
+		Guild gld = ((MessageReceivedEvent) misc).getGuild();
+		JSONObject data = Data.rawJSON;
+		final char[] illegalCharacters = new char[] {'@', '<', '>', '!', '#', ' '};
+		
+		// Obtain the ID
+		modifyThis = modifyThis.substring(modifyThis.indexOf(" ") + 1);
+		
+		// Clean the ID
+		for(char c: illegalCharacters) {
+			modifyThis = modifyThis.replaceAll(Character.toString(c), "");
+		}
+		
+		// Modify the data
+		data.getJSONObject(gld.getId()).getJSONObject("srvr_config").put("logging_channel", modifyThis);
+		
+		// Write the new data
+		if(Data.writeData(InitData.locationJSON, data.toString(), true, gld.getId())) {
+			MessageHandler.sendMessage(chn, "Logging channel is now set to " + gld.getTextChannelById(modifyThis).getAsMention());
+		} else {
+			MessageHandler.sendMessage(chn, "Logging channel failed to be set. Debug parse: " + modifyThis);
+			return false;
+		}
+		
+		return true;
 	}
 
 }
